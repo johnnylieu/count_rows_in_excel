8ff11db9e65ddbd2b46be0b78e50b575ac8f929a
Johnny <JohnnyLieu@yahoo.com>
2022-12-05 15:48:59 -0800

    done

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ import pandas as pd
 from openpyxl import load_workbook
 
 def count():
-    dir = r'C:/Users/Johnn/OneDrive/Desktop/Brain Corp/BJs/EOP Report/Localization Reports'
+    lines_for_each_file = []
+
+    dir = r'C:/Users/Johnn/OneDrive/Desktop/Brain Corp/BJs/EOP Report/Exception Reports'
 
     files = [f for f in os.listdir(dir) if os.path.isfile(f) and f != 'main.py' and f!= ".gitignore"]
+
     count = 0
     for file in files:
         df = pd.read_excel(file)
 
         print(len(df.index))
+        lines_for_each_file.append(len(df.index))
+    
+    print(sum(lines_for_each_file))
 
 def main():
     if __name__ == "__main__":
